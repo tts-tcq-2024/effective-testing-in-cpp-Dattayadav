b1b2acc33c268e05523728b9dade184343a67939
Dattayadav <87682187+Dattayadav@users.noreply.github.com>
2024-10-31 13:47:23 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,22 +1,35 @@
-#include <iostream>
-#include <assert.h>
+#include "tshirts.h"
 
-char size(int cms) {
-    char sizeName = '\0';
-    if(cms < 38) {
-        sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
-        sizeName = 'M';
-    } else if(cms > 42) {
-        sizeName = 'L';
-    }
-    return sizeName;
+namespace tshirts
+{
+    char tshirtsize::size(int cms)
+    {
+        char sizeName = '\0';
+        if (cms < 38)
+        {
+            sizeName = 'S';
+        }
+        else if (cms > 38 && cms < 42)
+        {
+            sizeName = 'M';
+        }
+        else if (cms > 42)
+        {
+            sizeName = 'L';
+        }
+        return sizeName;
+    };
 }
 
-int main() {
-    assert(size(37) == 'S');
-    assert(size(40) == 'M');
-    assert(size(43) == 'L');
-    std::cout << "All is well (maybe!)\n";
+int main()
+{
+    tshirts::tshirtsize *l_object{};
+    assert(l_object->size(37) == 'S');
+    assert(l_object->size(40) == 'M');
+    assert(l_object->size(43) == 'L');
+    // No sizename definition for size-> 38 and 42 in implementation.
+    assert(l_object->size(38) == 'S');
+    assert(l_object->size(42) == 'L');
+    std::cout << "All is not well\n";
     return 0;
 }
