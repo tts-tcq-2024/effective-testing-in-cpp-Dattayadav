9e6bc05e72751b7c15e833d9643832a1f8800439
Dattayadav <87682187+Dattayadav@users.noreply.github.com>
2024-10-31 13:43:06 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -1,32 +1,37 @@
-#include <iostream>
-#include <assert.h>
+#include "alerter.h"
 
-int alertFailureCount = 0;
+namespace alerter
+{
 
-int networkAlertStub(float celcius) {
-    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
-}
+    void Alerter::alertInCelcius(float farenheit)
+    {
+        float celcius = farenheitToCelcius(farenheit);
+        int returnCode = networkAlertStub(celcius);
+        if (returnCode != 200)
+        {
+            // non-ok response is not an error! Issues happen in life!
+            // let us keep a count of failures to report
+            // However, this code doesn't count failures!
+            // Add a test below to catch this bug. Alter the stub above, if needed.
+            alertFailureCount += 0;
+        }
+    }
 
-void alertInCelcius(float farenheit) {
-    float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
-    if (returnCode != 200) {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+    float Alerter::farenheitToCelcius(float farenheit)
+    {
+        return (farenheit - 32) * 5 / 9;
     }
 }
 
-int main() {
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
-    std::cout << alertFailureCount << " alerts failed.\n";
-    std::cout << "All is well (maybe!)\n";
+int main()
+{
+    alerter::Alerter l_alerter;
+    l_alerter.alertInCelcius(400.5);
+    l_alerter.alertInCelcius(303.6);
+    l_alerter.alertInCelcius(193.6);
+    std::cout << l_alerter.alertFailureCount << " alerts failed.\n";
+    assert(l_alerter.networkAlertStub(l_alerter.farenheitToCelcius(303.6)) == 500);
+    assert(l_alerter.alertFailureCount == 1);
+    std::cout << "All is not well \n";
     return 0;
 }
